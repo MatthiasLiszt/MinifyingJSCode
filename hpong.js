8286aae93de8d320c0e77155b336dc1eb3d0e20d
Matthias Liszt <mliszt.program@gmail.com>
2021-03-20 09:40:42 +0100

    files added

---
+++ hpong.js
@@ -0,0 +1,11 @@
+ş="style°ground°position:absolute;°px°black°width:°height:°ball°background-color:°paddle°score°top°div°id=°</div>°);°document.getElementById('°');°points°=0;°direction°onkey°init°=()=>{°Math.floor(°Math.random()°.left°};°reflect°>=°||°<=°falling°document.body.°();°.innerHTML°=parseInt(°);°";
+Ş=`ÿ='<ÿ>#Ā{āborder: 9Ă solid ă;Ą400Ă;ą300Ă;}#Ć{āĄ9Ă;ą9Ă;ćă}#Ĉ{āĄ36Ă;ą9Ă;ćă}#ĉ{āĊ:36Ă;right:81Ă;}</ÿ>';Ā='<ċ Č"Ā"><ċ Č"Ć">č<ċ Č"Ĉ">č<ċ Č"ĉ">čč';
+  document.write(ÿ+ĀĎĆ=ďĆĐĈ=ďĈĐĉ=ďĉĐđĒēĒĔ=9;ĕĆĖx=ėĘ*300)+21;y=ėĘ*100)+21;Ć.ÿ.Ċ=y;Ć.ÿę=x;ē={x:ėĘ*12),y:ėĘ*12)ĚĚĕĈĖĈ.ÿ.Ċ=264;Ĉ.ÿę=190;Ě
+  ě1ĖyģĆ.ÿ.ĊĤxģĆ.ÿęĤ ē.x=(xĜ400ĝxĞ0)?(ē.x*-1):ē.x;ē.y=(yĜ300ĝyĞ0)?(ē.y*-1):ē.y;đ=(y<285)?(++đ):0;ĉĢ=đ;Ć.ÿ.Ċ=y+ē.y;Ć.ÿę=x+ē.x;Ě
+  ě2Ėē.y*=-1;yģĆ.ÿ.ĊĤxģĆ.ÿęĤ Ć.ÿ.Ċ=y+ē.y;Ć.ÿę=x+ē.x;Ě ğĖyģĆ.ÿ.ĊĤxģĆ.ÿęĤ yB=y>0&&y<300;xB=x>0&&x<400;yyģĈ.ÿ.ĊĤxxģĈ.ÿęĤ
+  if((y>=(yy-7)&&y<=(yy+18))&&(x>=(xx-7)&&x<=(xx+36))){ě2();}if(yB&&xB){Ć.ÿ.Ċ=y+ē.y;Ć.ÿę=x+ē.x;}else{ě1();}Ě
+  ĠĔupĖĔ*=-1;Ě ĠĔdownĖ xģĈ.ÿęĤ Ĉ.ÿę=Math.abs((x+Ĕ)%364);Ě ĉĢĒĕĆġĕĈġsetInterval(ğ,162);`;
+ś=[];Ś=[];
+ş.split('°').map((x,i)=>{ś[String.fromCodePoint(0xff+i)]=x;});
+Ş.split('').map((x)=>{if(ś[x]!==undefined){Ś.push(ś[x])}else{Ś.push(x)}});
+eval(Ś.join(''));
